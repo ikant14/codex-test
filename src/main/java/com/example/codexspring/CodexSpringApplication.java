7dc721b8430491c074d84b9a44419b3752eca6c6
Irakli Kantaria <irakli.kantaria@viavoo.com>
2025-08-04 19:28:33 +0400

    init project

---
+++ src/main/java/com/example/codexspring/CodexSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.codexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codexspring/CodexSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codexspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
